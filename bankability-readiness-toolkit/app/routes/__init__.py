3b9dbe6b91201b0712535d405f3e641ebe85d9cc
femmyoniran <femmyoniran@gmail.com>
2026-02-20 16:42:54 -0700

    Delete bankability-readiness-toolkit directory

--- bankability-readiness-toolkit/app/routes/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.routes.main_routes import main_bp
-from app.routes.project_routes import project_bp
-from app.routes.analysis_routes import analysis_bp
-from app.routes.report_routes import report_bp
-
-__all__ = ["main_bp", "project_bp", "analysis_bp", "report_bp"]
--- bankability-readiness-toolkit/run.py
+++
@@ -1,9 +0,0 @@
-import os
-from app import create_app
-
-env = os.environ.get("FLASK_ENV", "development")
-app = create_app(env)
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
